3afffc4fce65b525ea87f68ca2f863abefd244e9
RandaAsim <randasanfora2015@gmai.com>
2023-08-10 14:29:04 -0700

    C programing

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - enter
+ *
+ * Description: C program
+ * Return: 0
+*/
+
+int main(void)
+{
+	printf("Size of a char: %u byte(s)\n", sizeof(shar));
+	printf("Size of a int: %u byte(s)\n", sizeof(int);
+			printf("Size of long int: %u byte(s)\n", sizeof(long int));
+			printf("Size of long long int: %u byte(s)\n", sizeof(long long it));
+			printf("Size of float: %u byte(s)\n", sizeof(float));
+			return (0);
+			}
