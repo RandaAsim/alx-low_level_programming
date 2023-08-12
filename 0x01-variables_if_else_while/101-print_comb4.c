7c49651372f7346eb520a9285e6c16de3094b3d1
RandaAsim <randasanfora2015@gmai.com>
2023-08-11 18:40:47 -0700

    C variables

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Description: prints all possible different combinations of three digits.
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int digit1 = 0;
+	int digit2;
+	int digit3;
+
+	while (digit1 <= 9)
+	{
+		digit2 = 0;
+		while (digit2 <= 9)
+		{
+			digit3 = 0;
+			while (digit3 <= 9)
+			{
+		if (digit1  != digit2 && digit1 < digit2 && digit2 != digit3 && digit2 < digit3)
+		{
+			putchar(digit1 + '0');
+			putchar(digit2 + '0');
+			putchar(digit3 + '0');
+			if (digit1 + digit2 + digit3 != 24)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		digit3++;
+	}
+	digit2++;
+}
+digit1++;
+}
+putchar('\n');
+return (0);
+}
