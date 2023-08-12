0e99599ae6b9751ef14673f326b7a997a4fcf352
RandaAsim <randasanfora2015@gmai.com>
2023-08-11 17:25:39 -0700

    C variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Description: prit all digit
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		digit++;
+	}
+	printf("\n");
+	return (0);
+}
