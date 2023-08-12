9cbe4602790119cbff327fa7fa7da4181f4c33c6
RandaAsim <randasanfora2015@gmai.com>
2023-08-11 17:56:25 -0700

    C variables

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - enrt
+ *
+ * Description: prints all possible combinations of single-digit numbers
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <=9)
+	{
+		putchar(digit + 0);
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
