f5754abf3c98aa28ade4f2555d17b1a1438b4bd1
RandaAsim <randasanfora2015@gmai.com>
2023-08-17 14:49:20 -0700

    functions_nested_loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,7 +5,7 @@
  * 		can only use _putchar three times
  *
  * Return: Always 0.
- */
+*/
 
 void more_numbers(void)
 {
@@ -16,7 +16,8 @@ void more_numbers(void)
 		for (count = 0; count <= 14; count++)
 		{
 			num = count;
-			if (cont > 9)
+
+			if (count > 9)
 			{
 				_putchar(1 + 48);
 				num = count % 10;
