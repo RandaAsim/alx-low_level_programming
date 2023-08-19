b58e1216f276f460171f7499eb10b735da20335c
RandaAsim <randasanfora2015@gmai.com>
2023-08-18 17:50:59 -0700

    function nested loop

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar -write the character c to stdout
+ * @c: the character to print
+ *
+ * Return: on success 1
+ *	on error 0
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
