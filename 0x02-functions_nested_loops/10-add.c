caafd4406e0399412691b2f5cfb3ec3c51c58ba5
RandaAsim <randasanfora2015@gmai.com>
2023-08-28 15:34:36 -0700

    functions nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two intigers
+ *
+ * @n1: input one
+ * @n2: seconde input
+ * Return: 0
+ */
+
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 time table
+ *
+ */
+
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0 ; i < 10; i++)
+	{
+		for (j = 0 ; j < 10 ; j++)
+		{
+			k = i * j;
+			if (k > 9)
+			{
+				_putchar((k / 10) + 48);
+				_putchar((k % 10) + 48);
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else if (k < 10)
+			{
+				if (j != 0)
+					_putchar(' ');
+				_putchar(k + 48);
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
