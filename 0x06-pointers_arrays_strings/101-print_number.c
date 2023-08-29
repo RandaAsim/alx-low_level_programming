d73bbaa3edf39a2e6b48fb01fe2cc1bf0564dee4
RandaAsim <randasanfora2015@gmai.com>
2023-08-28 19:16:40 -0700

    i will cry

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - function that print integer
+ * @n: integer
+ */
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -i;
+	}
+	if (i / 10)
+		print_number(i / 10);
+	_putchar(48 + (i % 10));
+}
