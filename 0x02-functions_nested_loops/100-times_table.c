f394c76d641eaf890ab157c72e07dc1d608b5e31
RandaAsim <randasanfora2015@gmai.com>
2023-08-28 18:07:51 -0700

    functions nested loops

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_time_table - function to print
+ * @n: inpur
+ */
+
+void print_times_table(int n)
+{
+	int prod, mul, num;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (num = 0; num <= n; ++num)
+		{
+			_putchar(48);
+			for (mul = 1; mul <= n; ++mul)
+			{
+				_putchar(',');
+				_putchar(' ');
+				prod = num * mul;
+
+				if (prod <= 9)
+					_putchar(' ');
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + 48);
+					_putchar((prod / 10) % 10 + 48);
+				}
+				else if (prod <= 99 && prod >= 10)
+					_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
+			}
+			_putchar('\n');
+		}
+	}
+}
