fea1c31f93fb4cac6740ee4ae915fa0336132e48
RandaAsim <randasanfora2015@gmai.com>
2023-08-30 14:55:50 -0700

    advance

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,68 +1,30 @@
 #include "main.h"
 
 /**
- * move_past_star - function
- * @s2: string2
- * Return: past pointer
- */
-
-char *move_past_star(char *s2)
-{
-	if (*s2 == '*')
-		return (move_past_star(s2 + 1));
-	else
-		return (s2);
-}
-
-/**
- * inception - function
+ * wildcmp - Compare strings
  * @s1: string1
  * @s2: string2
- * Return: 0 or 1
- */
-
-int inception(char *s1, char *s2)
-{
-	int r = 0;
-
-	if (*s1 == 0)
-		return (0);
-	if (*s1 == *s2)
-		r = r + wildcmp(s1 + 1, s2 + 1);
-	r = r + inception(s1 + 1, s2);
-	return (r);
-}
-
-/**
- * wildcmp - function
- * @s1: string1
- * @s2: string2
- * Return: 0 or 1
+ *  Return: 0
  */
 
 int wildcmp(char *s1, char *s2)
 {
-	int r = 0;
-
-	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
-		return (1);
-	if (*s1 == *s2)
+	if (*s1 == '\0')
 	{
-		if (!*s1)
-			return (1);
-		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
 	}
-	if (!*s1 || !s2)
-		return (0);
-	if (*s2 == 0)
+
+	if (*s2 == '*')
 	{
-		s2 = move_past_star(s2);
-		if (!*s2)
-			return (1);
-		if (*s1 == *s2)
-			r = r + wildcmp(s1 + 1, s2 + 1);
-		r = r + inception(s1, s2);
-		return (!!r);
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
 }
