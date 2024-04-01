e3621fd59e473216ce9d67bb981cbd4366ea1708
RandaAsim <randasanfora2015@gmail.com>
2024-04-01 07:08:08 -0700

    i hate C

--- 0x18-dynamic_libraries/main.c
+++ 0x18-dynamic_libraries/main.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always EXIT_SUCCESS.
- *     */
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
 int main(void)
 {
 	printf("%d\n", _strlen("My Dyn Lib"));
