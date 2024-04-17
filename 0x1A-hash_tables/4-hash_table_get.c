bb1fa61e7de303e3d0b7932356db2fd0b2c8c2ac
RandaAsim <randasanfora2015@gmail.com>
2024-04-17 03:44:35 -0700

    alx why C

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get the index of a key
+ * @ht: the hash table
+ * @key: the key to add
+ * Return: the value with the key
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (!ht || !ht->array || !ht->size ||
+			!key || strlen(key) == 0)
+		return (NULL);
+
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+
+	}
+
+	return (NULL);
+}
---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - retrieves a value associated with a key
+ * @ht: table to retrieve value from
+ *
+ * Return: value associated with key, or NULL if key cannot be found
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	char flag = 0;
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			flag = 1;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
